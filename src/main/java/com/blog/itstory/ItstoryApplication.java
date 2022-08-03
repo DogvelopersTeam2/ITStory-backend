40b1740365497490b096beefa3e29300bf4b3da8
vact19 <vact19@naver.com>
2022-08-04 00:50:31 +0900

    짜잔git status

---
+++ src/main/java/com/blog/itstory/ItstoryApplication.java
@@ -0,0 +1,13 @@
+package com.blog.itstory;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ItstoryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ItstoryApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/blog/itstory/ItstoryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.blog.itstory;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ItstoryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
