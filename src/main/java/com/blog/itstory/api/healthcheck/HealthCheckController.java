cd2d4f61f6c3ba37d147302294f01b2b98a0c1de
vact19 <vact19@naver.com>
2022-08-13 15:28:52 +0900

    feat: ELB HealthCheck 경로 매핑과 반환 설정

---
+++ src/main/java/com/blog/itstory/api/healthcheck/HealthCheckController.java
@@ -0,0 +1,14 @@
+package com.blog.itstory.api.healthcheck;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HealthCheckController {
+
+    @GetMapping("/")
+    public ResponseEntity<String> healthCheck(){
+        return ResponseEntity.ok("짜자잔");
+    }
+}
